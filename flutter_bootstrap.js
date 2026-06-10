4a3b8dc0c054f8c47a9b8255980e4081b989f382
amber-null <ambernullzzz@gmail.com>
2026-06-10 14:23:08 +0000

    Deploying to gh-pages from @ amber-null/TikDown@c6f075669c068aa843168e0720f0ed8fecc0519d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "877245408" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "597363214" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
